ff4ccb7cf8c9cb1965b3f303338f21d46925c73f
zile <user1@user1s-MacBook-Pro.local>
2019-12-01 12:14:32 -0500

    added scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import './App.css';
-import HomePage from './HomePage';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage";
 
 function App() {
-  return (
-    < HomePage />
-  )
+	return <HomePage />;
 }
 
 export default App;
