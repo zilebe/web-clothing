b1c1ed514ee7e338df1ab02e09cd61683a33c815
zilebe <zile93@hotmail.com>
2019-12-16 16:41:22 -0500

    added signin and signup pages with firebase

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input";
 import CustomButton from "../custom-button/custom-button";
 import "./sign-in.scss";
-import { signInWithGoogle } from "../../firebase/firebase";
+import { auth, signInWithGoogle } from "../../firebase/firebase";
 
 class SignIn extends React.Component {
 	constructor(props) {
@@ -14,10 +14,15 @@ class SignIn extends React.Component {
 		};
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
-
-		this.setState({ email: "", password: "" });
+		const { email, password } = this.state;
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: "" });
+		} catch (error) {
+			console.log(error);
+		}
 	};
 
 	handleChange = event => {
